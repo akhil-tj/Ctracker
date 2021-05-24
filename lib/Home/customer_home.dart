ba0d0f098617f0927eb53d6260e0cac20ac0f108
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-05-24 10:08:36 +0530

    customer home

--- lib/Home/customer_home.dart
+++ lib/Home/customer_home.dart
@@ -86,101 +86,73 @@ class _CustomerHomeState extends State<CustomerHome> {
     );
   }
 
+  Future MerchantProfile() {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    final User user = auth.currentUser;
+    final uid = user.uid;
+    DatabaseReference merchantsprofileRef =
+        FirebaseDatabase.instance.reference().child("merchantusers").child(uid);
+
+    return merchantsprofileRef.once();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, 'qr_code_scanner');
-        },
-        backgroundColor: vilot,
-        child: Icon(Icons.qr_code),
-      ),
-      appBar: AppBar(
-        // leading: Container(
-        //   padding: EdgeInsets.all(13),
-        //   child: SvgPicture.asset(
-        //     'assets/menu.svg',
-        //     color: Colors.white,
-        //   ),
-        // ),
-        backgroundColor: vilot,
-        title: Text(
-          'My Footprints',
-          style: h2,
-        ),
-        automaticallyImplyLeading: false,
-        actions: [
-          CircleAvatar(
-            //backgroundColor: Colors.white,
-            radius: 18.0,
-            backgroundImage: AssetImage(
-              'assets/Ellipse 1.png',
+    return FutureBuilder<dynamic>(
+        future: MerchantProfile(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return CircularProgressIndicator();
+          }
+          return Scaffold(
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                Navigator.pushNamed(context, 'qr_code_scanner');
+              },
+              backgroundColor: vilot,
+              child: Icon(Icons.qr_code),
             ),
-          ),
-          SizedBox(
-            width: 16,
-          )
-        ],
-      ),
-      body: Container(
-        height: double.infinity,
-        child: FirebaseAnimatedList(
-          query: _ref,
-          itemBuilder: (BuildContext context, DataSnapshot snapshot,
-              Animation<double> animation, int index) {
-            Map contact = snapshot.value;
-            return _buildContactItem(contact: contact);
-          },
-        ),
-      ),
-    );
+            appBar: AppBar(
+              // leading: Container(
+              //   padding: EdgeInsets.all(13),
+              //   child: SvgPicture.asset(
+              //     'assets/menu.svg',
+              //     color: Colors.white,
+              //   ),
+              // ),
+              backgroundColor: vilot,
+              title: Text(
+                'My Footprints',
+                style: h2,
+              ),
+              automaticallyImplyLeading: false,
+              actions: [
+                CircleAvatar(
+                  //backgroundColor: Colors.white,
+                  radius: 18.0,
+                  backgroundImage: NetworkImage(
+                      'https://firebasestorage.googleapis.com/v0/b/c-tracker-2021.appspot.com/o/files%2F' +
+                          snapshot.data.value['email'] +
+                          '?alt=media&token=a2d4e1d0-202e-49c4-86b7-59df731559b3'),
+                  backgroundColor: Colors.transparent,
+                ),
+                SizedBox(
+                  width: 16,
+                )
+              ],
+            ),
+            body: Container(
+              height: double.infinity,
+              child: FirebaseAnimatedList(
+                query: _ref,
+                itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                    Animation<double> animation, int index) {
+                  Map contact = snapshot.value;
+                  return _buildContactItem(contact: contact);
+                },
+              ),
+            ),
+          );
+        });
   }
 }
-
-// ignore: must_be_immutable
-// class CustomerHomeBody extends StatelessWidget {
-//   List<CustomerHomeListTile> footPrintDetails = [
-//     CustomerHomeListTile(
-//       customerAvatar: 'assets/Ellipse 2.png',
-//       cusName: 'Take Off Sports',
-//       cusDate: '15 May 2021',
-//       cusTime: '10:30am',
-//     ),
-//     CustomerHomeListTile(
-//       customerAvatar: 'assets/Ellipse 3.png',
-//       cusName: 'Broklie Store',
-//       cusDate: '15 May 2021',
-//       cusTime: '10:15am',
-//     ),
-//     CustomerHomeListTile(
-//       customerAvatar: 'assets/Ellipse 4.png',
-//       cusName: 'Memu Restaurent',
-//       cusDate: '12 May 2021',
-//       cusTime: '1:38pm',
-//     ),
-//     CustomerHomeListTile(
-//       customerAvatar: 'assets/Ellipse 5.png',
-//       cusName: 'Internet Cafe',
-//       cusDate: '29 April 2021',
-//       cusTime: '11:10am',
-//     ),
-//     CustomerHomeListTile(
-//       customerAvatar: 'assets/Ellipse 6.png',
-//       cusName: 'Local Store',
-//       cusDate: '25 April 2021',
-//       cusTime: '4:30pm',
-//     ),
-//   ];
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//         shrinkWrap: true,
-//         physics: NeverScrollableScrollPhysics(),
-//         itemCount: footPrintDetails.length,
-//         itemBuilder: (context, index) {
-//           return footPrintDetails[index];
-//         });
-//   }
-// }
